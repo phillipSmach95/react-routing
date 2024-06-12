80b5d9163e70dd8440a5b0f17b1270f65867e9c5
phillipSmach95 <smach.phillip@gmail.com>
2024-06-12 16:59:00 +0200

    mostly done

--- react-routing/src/Detail.jsx
+++ react-routing/src/Detail.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { getProduct } from "./services/productService";
+import useFetch from "./services/useFetch";
 
-export default function Detail() {
-  return <h1>Detail</h1>;
+export default function Detail(id) {
+  const { data: product, loading, error } = useFetch(
+    `products?category=boards ${id}`
+  );
+
+    return (
+      <>
+      
+      <h1>Detail</h1>
+      <div id="detail">
+        <h1>{product.name}</h1>
+        <p>{product.description}</p>
+        <p id="price">${product.price}</p>
+        <img src={`/images/${product.image}`} alt={product.category} />
+      </div>
+      </>
+    
+    );
+  
+ 
 
   // TODO: Display these products details
-  // return (
-  //   <div id="detail">
-  //     <h1>{product.name}</h1>
-  //     <p>{product.description}</p>
-  //     <p id="price">${product.price}</p>
-  //     <img src={`/images/${product.image}`} alt={product.category} />
-  //   </div>
-  // );
+  
 }
--- react-routing/src/Header.jsx
+++ react-routing/src/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -8,6 +9,9 @@ export default function Header() {
           <li>
             <img alt="Just skate." src="/images/logo.png" />
           </li>
+          <li><Link to={"/"} >Home</Link></li>
+          <li><NavLink to={"/boards"}>Boards</NavLink></li>
+          <li><NavLink to={"/carts"}>carts</NavLink></li>
         </ul>
       </nav>
     </header>
